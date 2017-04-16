f419de278ab38b050c0806fc8cb040b76c2e4b52
drdhaval2785 <drdhaval2785#gmail.com>
2017-04-16 13:34:31 +0530

    initial commit

---
+++ make_babylon.py
@@ -0,0 +1,155 @@
+# This Python file uses the following encoding: utf-8
+"""
+Usage:
+python make_babylon.py pathToDicts dictId
+e.g.
+python make_babylon.py ../../Cologne_localcopy md
+"""
+import re,codecs,sys
+from lxml import etree
+import transcoder
+import datetime
+
+# Function to return timestamp
+def timestamp():
+	return datetime.datetime.now()
+
+def add_tags1(x):
+	global prevsutra
+	m = re.search(u'^(.*)॥([१२३४५६७८९०। /-]*)॥',x)
+	sutra = m.group(1).strip()
+	num = m.group(2).strip()
+	current_sutra = num.split(u'।')
+	print current_sutra
+	if len(current_sutra) != 3:
+		print current_sutra
+		prevsutra = current_sutra
+		exit(0)
+	result = '\n\n'+num+'|'+sutra+'|'+sutra+' '+num+'|'+num+' '+sutra+'\n'+sutra+' '+num+' <BR> '
+	result = result.replace(u'।','.')
+	return result
+
+	
+
+if __name__=="__main__":
+	pathToDicts = sys.argv[1]
+	dictId = sys.argv[2]
+	production = sys.argv[3]
+	#dictList = ['acc','ae','ap','ap90','ben','bhs','bop','bor','bur','cae','ccs','gra','gst','ieg','inm','krm','mci','md','mw','mw72','mwe','pd','pe','pgn','pui','pw','pwg','sch','shs','skd','snp','stc','vcp','vei','wil','yat']
+
+	meaningseparator = {'acc':('([ .])--','\g<1>BREAK --'), 'md':(';',';BREAK'), 'ap90':('<b>[-]{2}([0-9]+)</b>','BREAK<b>\g<1></b>'), 'ben':(' <b>','BREAK <b>'), 'bhs':('([(]<b>[0-9]+</b>[)])','BREAK\g<1>'), 'bor':(' <br>',' BREAK'), 'bur':(';;','BREAK'), 'cae':(';',';BREAK'), 'ccs':(';',';BREAK'), 'gra':('<P1></P1>','BREAK'), 'gst':('<P></P>','BREAK'), 'ieg':('; ',';BREAK'), 'mci':('<b>','BREAK<b>'), 'mw72':('<i>--','BREAK<i>--'), 'mwe':('.--','BREAK--'), 'ap':('</lb>[.]','</lb>BREAK')}
+	if dictId in meaningseparator:
+		instr = meaningseparator[dictId][0]
+		outstr = meaningseparator[dictId][1]
+	inputfile = pathToDicts+'/'+dictId+'.xml'
+	tree = etree.parse(inputfile)
+	hw = tree.xpath("/"+dictId+"/H1/h/key1")
+	key2s = tree.xpath("/"+dictId+"/H1/h/key2")
+	entry =  tree.xpath("/"+dictId+"/H1/body")
+	if production == '0':
+		outputfile = codecs.open('output/'+dictId+'.babylon','w','utf-8')
+	elif production == '1':
+		outputfile = codecs.open('production/'+dictId+'.babylon','w','utf-8')
+	counter = 0
+	for x in xrange(len(hw)):
+		heading1 = etree.tostring(hw[x], method='text', encoding='utf-8')
+		key2 = etree.tostring(key2s[x], method='text', encoding='utf-8')
+		key2 = key2.decode('utf-8')
+		if counter % 1000 == 0:
+			print counter
+		counter += 1
+		#print heading1
+		if dictId not in ['ae','mwe','bor']:
+			heading = transcoder.transcoder_processString(heading1,'slp1','deva')
+		else:
+			heading = heading1
+		#text = etree.tostring(entry[x], method='text', encoding='utf-8')
+		html = etree.tostring(entry[x], method='html', encoding='utf-8')
+		html = re.sub('\[Page[0-9+ abc.-]+\]','',html)
+		if dictId in ['mwe','ap']:
+			html = html.replace('<lb></lb>',' ')
+		else:
+			html = html.replace('<lb></lb>','')
+		if dictId in ['pd']:
+			html = html.replace('<br>','')
+		if dictId in ['pw','pwg']:
+			html = re.sub(' <gram','BREAK<gram',html)
+			html = re.sub('([^(])<divm','BREAK\g<1><divm',html)
+		html = html.replace('<b>--Comp.</b>','BREAK<b>--Comp.</b>BREAK') # ap90
+		#print html
+		html = html.decode('utf-8')
+		html = html.replace(u'<b><s>º',u'BREAK<b><s>º') # ap90
+		#print html.encode('utf-8')
+		if dictId in meaningseparator and re.search(instr,html):
+			html = re.sub(instr,outstr,html)
+		html = re.sub(u'[(](<b><s>--[a-zA-Z]+</s>)',u'BREAK\g<1>',html) # ap90
+		sanskrittext = re.findall('<s>([^<]*)</s>',html)
+		html = re.sub(u'(<s>--[a-zA-Z]+</s>)',u'BREAK\g<1>',html) # ap90
+		for sans in sanskrittext:
+			html = html.replace('<s>'+sans+'</s>','<s>'+transcoder.transcoder_processString(sans,'slp1','deva')+'</s>')
+		if dictId in ['ben']:
+			html = html.replace(' <i>','BREAK <i>')
+			html = html.replace('i. e.BREAK <i>','i.e. <i>')
+		if dictId in ['pd']:
+			html = re.sub(u'(¦[^<]*)<i>','\g<1>',html)
+			italictext = re.findall('<i>([^<]*)</i>',html)
+			for ital in italictext:
+				rep = transcoder.transcoder_processString(ital,'as','slp1')
+				rep = rep.replace(u'ç',u'S')
+				rep = rep.replace(u'Ç',u'S')
+				rep = transcoder.transcoder_processString(rep,'slp1','deva')
+				html = html.replace('<i>'+ital+'</i>','<i>'+rep+'</i>')
+		if dictId in ['bur']:
+			italictext = re.findall('<i>([^<]*)</i>',html)
+			for ital in italictext:
+				rep = transcoder.transcoder_processString(ital,'roman','slp1')
+				rep = rep.replace(u'ç',u'S')
+				rep = rep.replace(u'Ç',u'S')
+				rep = transcoder.transcoder_processString(rep,'slp1','deva')
+				html = html.replace('<i>'+ital+'</i>','<i>'+rep+'</i>')
+		if dictId in ['acc','ap90','ben','bhs','bur','cae','ccs','gra','gst','ieg','inm','mci','mw72','pd','pe','pgn','pui','pwg']:
+			html = transcoder.transcoder_processString(html,'as','roman')
+			html = html.replace(u'ç',u'ś')
+			html = html.replace(u'Ç',u'Ś')
+			html = html.replace(u'¦',u'') # ccs
+			html = html.replace(u'[Pagē-',u'[Page 1-')
+			html = html.replace(u'[Pagė',u'[Page 1-')
+			html = html.replace(u'C¤:',u'Comm.')
+			html = html.replace(u'n1',u'ṅ')
+			html = html.replace(u'm5',u'ṃ') # ben
+			if not dictId in ['pui']:
+				html = html.replace(' --','BREAK --') # ap90
+			html = html.replace(' <b>1</b>','BREAK<b>1</b>')
+			html = re.sub('(<i>--[^<]*</i>)','BREAK\g<1>',html)
+			html = html.replace('] ',']BREAK')
+			html = html.replace('<s>(</s>','(')
+			html = html.replace('<s>) </s>',') ')
+			html = html.replace(u'ṛi',u'ṛ')
+		if dictId in ['bur']:
+			html = html.replace(u'|',u'')
+		if dictId in ['cae']:
+			html = html.replace(u'·',u'')
+		if dictId in ['gst']:
+			html = html.replace(' ^','BREAK ^')
+		if dictId in ['mwe']:
+			html = re.sub(u'[)]([^ ,.;\n])',u') \g<1>',html)
+		html = html.replace('- ','')
+		html = re.sub('[ ]+',' ',html)
+		html = html.replace('&amp;','&')
+		html = html.replace(u'î',u'ī')
+		html = html.replace(u'â',u'ā')
+		html = re.sub('[<][^>]*[>]','',html)
+		if dictId in ['ap']:
+			html = html.replace('- -','-')
+		if dictId in ['pd']:
+			html = re.sub('^[.]','',html)
+		html = html.replace('BREAK','<BR>')
+		if dictId in ['pw','pwg']:
+			html = transcoder.transcoder_processString(key2,'slp1','deva')+'<BR>'+html
+		html = html.replace('<BR><BR>','<BR>')
+		if production == '0':
+			html = html.replace('<BR>','\n')
+		outputfile.write(heading+'\n')
+		outputfile.write(html+'\n\n')
+	outputfile.close()
+		
---
+++ updatexml.sh
@@ -0,0 +1,15 @@
+# shell script takes a single argument, a dictionary code
+# convert shell script argument to lower case
+dictList=(acc ae ap ap90 ben bhs bop bor bur cae ccs gra gst ieg inm krm mci md mw mw72 mwe pd pe pgn pui pw pwg sch shs skd snp stc vcp vei wil yat)
+for DICT in "${dictList[@]}"
+do
+echo "downloading "$DICT"_xml.zip ..."
+curl -o input/zips/"$DICT"_xml.zip http://s3.amazonaws.com/sanskrit-lexicon/blobs/"$DICT"_xml.zip
+done
+
+cd input/extracted
+for DICT in "${dictList[@]}"
+do
+echo "unzipping "$DICT"_xml.zip ..."
+unzip -o ../zips/"$DICT"_xml.zip
+done
