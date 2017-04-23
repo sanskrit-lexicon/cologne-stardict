37e813752ea31ac33185de39be5e9eec509ba985
drdhaval2785 <drdhaval2785#gmail.com>
2017-04-23 19:12:34 +0530

    LICENCE added

--- make_babylon.py
+++ make_babylon.py
@@ -14,30 +14,44 @@ import datetime
 def timestamp():
 	return datetime.datetime.now()
 
-def add_tags1(x):
-	global prevsutra
-	m = re.search(u'^(.*)॥([१२३४५६७८९०। /-]*)॥',x)
-	sutra = m.group(1).strip()
-	num = m.group(2).strip()
-	current_sutra = num.split(u'।')
-	print current_sutra
-	if len(current_sutra) != 3:
-		print current_sutra
-		prevsutra = current_sutra
-		exit(0)
-	result = '\n\n'+num+'|'+sutra+'|'+sutra+' '+num+'|'+num+' '+sutra+'\n'+sutra+' '+num+' <BR> '
-	result = result.replace(u'।','.')
-	return result
+# Function to read normalized headwords (hwnorm1c.txt) into a dict
+# Returns a dict with headword as key and the list of associated headwords as value.
+def readhwnorm1c():
+	fin = codecs.open('input/hwnorm1c.txt','r','utf-8')
+	lines = fin.readlines()
+	output = {}
+	for line in lines:
+		line = line.strip()
+		chunks = line.split(';')
+		words = []
+		if len(chunks) > 1:
+			baseword = chunks[0].split(':')[0]
+			for chunk in chunks[1:]:
+				worddictsep = chunk.split(':')
+				word = worddictsep[0]
+				dicts = worddictsep[-1].split(',')
+				if not baseword == word:
+					result = [baseword,word]
+					output[word] = (result,dicts)
+	return output
 
-	
-
+def licencetext(dictId):
+	fin = codecs.open('../Cologne_localcopy/'+dictId+'/'+dictId+'txt/'+dictId+'header.xml','r','utf-8')
+	data = fin.read()
+	fin.close()
+	return data
 if __name__=="__main__":
 	pathToDicts = sys.argv[1]
 	dictId = sys.argv[2]
 	production = sys.argv[3]
 	#dictList = ['acc','ae','ap','ap90','ben','bhs','bop','bor','bur','cae','ccs','gra','gst','ieg','inm','krm','mci','md','mw','mw72','mwe','pd','pe','pgn','pui','pw','pwg','sch','shs','skd','snp','stc','vcp','vei','wil','yat']
-
-	meaningseparator = {'acc':('([ .])--','\g<1>BREAK --'), 'md':(';',';BREAK'), 'ap90':('<b>[-]{2}([0-9]+)</b>','BREAK<b>\g<1></b>'), 'ben':(' <b>','BREAK <b>'), 'bhs':('([(]<b>[0-9]+</b>[)])','BREAK\g<1>'), 'bor':(' <br>',' BREAK'), 'bur':(';;','BREAK'), 'cae':(';',';BREAK'), 'ccs':(';',';BREAK'), 'gra':('<P1></P1>','BREAK'), 'gst':('<P></P>','BREAK'), 'ieg':('; ',';BREAK'), 'mci':('<b>','BREAK<b>'), 'mw72':('<i>--','BREAK<i>--'), 'mwe':('.--','BREAK--'), 'ap':('</lb>[.]','</lb>BREAK')}
+	
+	licence = licencetext(dictId).strip()
+	licence = licence.replace('\n','<BR>')
+	# Read a list of normalized headwords. See https://github.com/sanskrit-coders/stardict-sanskrit/issues/66.
+	hwnormlist = readhwnorm1c()
+	
+	meaningseparator = {'acc':('([ .])--','\g<1>BREAK --'), 'md':(';',';BREAK'), 'ap90':('<b>[-]{2}([0-9]+)</b>','BREAK<b>\g<1></b>'), 'ben':(' <b>','BREAK <b>'), 'bhs':('([(]<b>[0-9]+</b>[)])','BREAK\g<1>'), 'bor':(' <br>',' BREAK'), 'bur':(';;','BREAK'), 'cae':(';',';BREAK'), 'ccs':(';',';BREAK'), 'gra':('<P1></P1>','BREAK'), 'gst':('<P></P>','BREAK'), 'ieg':('; ',';BREAK'), 'mci':('<b>','BREAK<b>'), 'mw72':('<i>--','BREAK<i>--'), 'mwe':('.--','BREAK--'), 'ap':('<lb></lb>[.]','<lb></lb>BREAK'), 'pui':('</F>','</F>BREAK'), 'shs':('([).]) ([0-9nmf]+[.])','\g<1>BREAK \g<2>'), 'snp':('<P></P>','BREAK<P></P>'), 'stc':(';',';BREAK'), 'wil':(' ([mfn]+)[.]','BREAK\g<1>.'), 'yat':('<i>','BREAK<i>')}
 	if dictId in meaningseparator:
 		instr = meaningseparator[dictId][0]
 		outstr = meaningseparator[dictId][1]
@@ -50,6 +64,11 @@ if __name__=="__main__":
 		outputfile = codecs.open('output/'+dictId+'.babylon','w','utf-8')
 	elif production == '1':
 		outputfile = codecs.open('production/'+dictId+'.babylon','w','utf-8')
+		
+	# Write licence text
+	outputfile.write('LICENCE\n')
+	outputfile.write(unicode(licence)+u'\n\n')
+
 	counter = 0
 	for x in xrange(len(hw)):
 		heading1 = etree.tostring(hw[x], method='text', encoding='utf-8')
@@ -58,15 +77,26 @@ if __name__=="__main__":
 		if counter % 1000 == 0:
 			print counter
 		counter += 1
-		#print heading1
+		if heading1 in hwnormlist and dictId.upper() in hwnormlist[heading1][1]:
+			possibleheadings = hwnormlist[heading1][0]
+			print possibleheadings
+		else:
+			possibleheadings = [heading1]
+		"""
+		if len(possibleheadings) > 1:
+			print possibleheadings
+		"""
 		if dictId not in ['ae','mwe','bor']:
-			heading = transcoder.transcoder_processString(heading1,'slp1','deva')
+			heading = '|'.join([transcoder.transcoder_processString(head,'slp1','deva') for head in possibleheadings])
 		else:
 			heading = heading1
 		#text = etree.tostring(entry[x], method='text', encoding='utf-8')
 		html = etree.tostring(entry[x], method='html', encoding='utf-8')
 		html = re.sub('\[Page[0-9+ abc.-]+\]','',html)
-		if dictId in ['mwe','ap']:
+		if dictId in ['mwe','skd','vcp']:
+			html = html.replace('<lb></lb>',' ')
+		elif dictId in ['ap']:
+			html = re.sub('<lb></lb>[.]<b>','BREAK<b>',html)
 			html = html.replace('<lb></lb>',' ')
 		else:
 			html = html.replace('<lb></lb>','')
@@ -75,23 +105,41 @@ if __name__=="__main__":
 		if dictId in ['pw','pwg']:
 			html = re.sub(' <gram','BREAK<gram',html)
 			html = re.sub('([^(])<divm','BREAK\g<1><divm',html)
+		if dictId in ['sch']:
+			html = re.sub('\[Schµ[0-9]+\]','',html)
+			html = re.sub('€[0-9]+','',html)
+			html = html.replace(' -- ','BREAK -- ')
+		if dictId in ['acc']:
+			html = html.replace('<br>','BREAK\t')
 		html = html.replace('<b>--Comp.</b>','BREAK<b>--Comp.</b>BREAK') # ap90
-		#print html
+		if dictId in ['wil']:
+			html = html.replace('¦ ','BREAK')
+			html = html.replace('<body>.<s>','<body><s>')
+			html = html.replace('.E.','BREAK.E.')
+			html = html.replace('<br>','BREAK')
 		html = html.decode('utf-8')
+		if dictId in ['wil']:
+			html = html.replace(u'²',u'\t')
+		if dictId in ['ap']:
+			html = html.replace(u'.²',u'BREAK\t')
+			html = html.replace(u'.³','BREAK\t\t')
 		html = html.replace(u'<b><s>º',u'BREAK<b><s>º') # ap90
 		#print html.encode('utf-8')
+		
 		if dictId in meaningseparator and re.search(instr,html):
 			html = re.sub(instr,outstr,html)
 		html = re.sub(u'[(](<b><s>--[a-zA-Z]+</s>)',u'BREAK\g<1>',html) # ap90
+		if dictId in ['ben']:
+			html = html.replace(u'¤10',u'') # BEN usually has dIrga mentioned and hrasva being mentioned by breve.
 		sanskrittext = re.findall('<s>([^<]*)</s>',html)
 		html = re.sub(u'(<s>--[a-zA-Z]+</s>)',u'BREAK\g<1>',html) # ap90
 		for sans in sanskrittext:
 			html = html.replace('<s>'+sans+'</s>','<s>'+transcoder.transcoder_processString(sans,'slp1','deva')+'</s>')
 		if dictId in ['ben']:
-			html = html.replace(' <i>','BREAK <i>')
+			html = re.sub('<i>([^<]*[-][,])</i>','BREAK<i>\g<1></i>',html)
 			html = html.replace('i. e.BREAK <i>','i.e. <i>')
 		if dictId in ['pd']:
-			html = re.sub(u'(¦[^<]*)<i>','\g<1>',html)
+			html = re.sub(u'(¦[^<]*)<i>','\g<1>',html) # pd
 			italictext = re.findall('<i>([^<]*)</i>',html)
 			for ital in italictext:
 				rep = transcoder.transcoder_processString(ital,'as','slp1')
@@ -107,7 +155,37 @@ if __name__=="__main__":
 				rep = rep.replace(u'Ç',u'S')
 				rep = transcoder.transcoder_processString(rep,'slp1','deva')
 				html = html.replace('<i>'+ital+'</i>','<i>'+rep+'</i>')
-		if dictId in ['acc','ap90','ben','bhs','bur','cae','ccs','gra','gst','ieg','inm','mci','mw72','pd','pe','pgn','pui','pwg']:
+		if dictId in ['snp','stc']:
+			italictext = re.findall('<i>([^<]*)</i>',html)
+			for ital in italictext:
+				rep = transcoder.transcoder_processString(ital,'as','slp1')
+				rep = rep.replace(u'ç',u'S')
+				rep = rep.replace(u'Ç',u'S')
+				rep = transcoder.transcoder_processString(rep,'slp1','deva')
+				html = html.replace('<i>'+ital+'</i>','<i>'+rep+'</i>')
+		if dictId in ['stc','vei']:
+			italictext = re.findall('<body><b>([^<]*)</b>',html)
+			for ital in italictext:
+				rep = ital.lower()
+				rep = transcoder.transcoder_processString(rep,'as','slp1')
+				rep = rep.replace(u'ç',u'S')
+				rep = rep.replace(u'Ç',u'S')
+				rep = transcoder.transcoder_processString(rep,'slp1','deva')
+				html = html.replace('<body><b>'+ital+'</b>','<body><b>'+rep+'</b>BREAK')
+				html = re.sub(u'<body><b>([^<]*)।</b>',u'<body><b>\g<1></b>',html) # vei
+		if dictId in ['ben']:
+			italictext = re.findall('<i>([^<]*)</i>',html)
+			for ital in italictext:
+				rep = ital.lower()
+				rep = rep.replace('ch','c')
+				rep = rep.replace('sh','z')
+				rep = transcoder.transcoder_processString(rep,'as','slp1')
+				rep = rep.replace(u'ç',u'S')
+				rep = rep.replace(u'Ç',u'S')
+				rep = transcoder.transcoder_processString(rep,'slp1','deva')
+				html = html.replace('<i>'+ital+'</i>','<i>'+rep+'</i>')
+				#html = re.sub('<i>([^<]*[-][,])</i>','BREAK<i>\g<1></i>',html)
+		if dictId in ['acc','ap90','ben','bhs','bur','cae','ccs','gra','gst','ieg','inm','mci','mw72','pd','pe','pgn','pui','pwg','vei']:
 			html = transcoder.transcoder_processString(html,'as','roman')
 			html = html.replace(u'ç',u'ś')
 			html = html.replace(u'Ç',u'Ś')
@@ -125,8 +203,12 @@ if __name__=="__main__":
 			html = html.replace('<s>(</s>','(')
 			html = html.replace('<s>) </s>',') ')
 			html = html.replace(u'ṛi',u'ṛ')
+			
 		if dictId in ['bur']:
 			html = html.replace(u'|',u'')
+		if dictId in ['stc']:
+			html = transcoder.transcoder_processString(html,'as','roman')
+			html = html.replace('|','')
 		if dictId in ['cae']:
 			html = html.replace(u'·',u'')
 		if dictId in ['gst']:
--- redo.sh
+++ redo.sh
@@ -1,9 +1,11 @@
+echo "Copying latest hwnorm1c.txt file."
+echo "It presumes that the hwnorm1 repository is sibling of cologne-stardict repository."
+cp ../hwnorm1/ejf/hwnorm1c/hwnorm1c.txt input/hwnorm1c.txt
+
+echo
 dictList=(acc ae ap ap90 ben bhs bop bor bur cae ccs gra gst ieg inm krm mci md mw mw72 mwe pd pe pgn pui pw pwg sch shs skd snp stc vcp vei wil yat)
 for Val in "${dictList[@]}"
 do
 	echo 'Started' $Val 'handling'.
 	python make_babylon.py input/extracted/pywork $Val $1
 done
-
-sh move_to_stardict.sh
-
