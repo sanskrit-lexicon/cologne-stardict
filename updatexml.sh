3b881676f77d11c7e93d28779d0eea2e44ad1caf
drdhaval2785 <drdhaval2785#gmail.com>
2017-04-28 13:39:25 +0530

    bash shebang

--- updatexml.sh
+++ updatexml.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 dictList=(acc ae ap ap90 ben bhs bop bor bur cae ccs gra gst ieg inm krm mci md mw mw72 mwe pd pe pgn pui pw pwg sch shs skd snp stc vcp vei wil yat)
 for DICT in "${dictList[@]}"
 do
@@ -15,3 +16,4 @@ done
 echo "Copying latest hwnorm1c.txt file."
 echo "It presumes that the hwnorm1 repository is sibling of cologne-stardict repository."
 cp ../hwnorm1/ejf/hwnorm1c/hwnorm1c.txt input/hwnorm1c.txt
+
