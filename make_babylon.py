aedac9d56615fe0812de28dddff2ace72fb06420
drdhaval2785 <drdhaval2785#gmail.com>
2017-04-28 14:52:41 +0530

    script modified

--- make_babylon.py
+++ make_babylon.py
@@ -5,7 +5,7 @@ python make_babylon.py pathToDicts dictId
 e.g.
 python make_babylon.py ../../Cologne_localcopy md
 """
-import re,codecs,sys
+import re,codecs,sys,os.path
 from lxml import etree
 import transcoder
 import datetime
@@ -35,11 +35,14 @@ def readhwnorm1c():
 					output[word] = (result,dicts)
 	return output
 
-def licencetext(dictId):
-	fin = codecs.open('../Cologne_localcopy/'+dictId+'/'+dictId+'txt/'+dictId+'header.xml','r','utf-8')
-	data = fin.read()
-	fin.close()
-	return data
+def licencetext(licencefile):
+	if os.path.isfile(licencefile):
+		fin = codecs.open(licencefile,'r','utf-8')
+		data = fin.read()
+		fin.close()
+		return data
+	else:
+		return 'Not specified.'
 
 if __name__=="__main__":
 	pathToDicts = sys.argv[1]
@@ -47,8 +50,6 @@ if __name__=="__main__":
 	production = sys.argv[3]
 	#dictList = ['acc','ae','ap','ap90','ben','bhs','bop','bor','bur','cae','ccs','gra','gst','ieg','inm','krm','mci','md','mw','mw72','mwe','pd','pe','pgn','pui','pw','pwg','sch','shs','skd','snp','stc','vcp','vei','wil','yat']
 	
-	licence = licencetext(dictId).strip()
-	licence = licence.replace('\n','<BR>')
 	# Read a list of normalized headwords. See https://github.com/sanskrit-coders/stardict-sanskrit/issues/66.
 	hwnormlist = readhwnorm1c()
 	
@@ -57,6 +58,8 @@ if __name__=="__main__":
 		instr = meaningseparator[dictId][0]
 		outstr = meaningseparator[dictId][1]
 	inputfile = pathToDicts+'/'+dictId+'.xml'
+	licence = licencetext(pathToDicts+'/'+dictId+'header.xml').strip()
+	licence = licence.replace('\n','<BR>')
 	tree = etree.parse(inputfile)
 	hw = tree.xpath("/"+dictId+"/H1/h/key1")
 	key2s = tree.xpath("/"+dictId+"/H1/h/key2")
