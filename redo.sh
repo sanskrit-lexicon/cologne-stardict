9e4718fd01a346e9b34413765733ed58b8829b22
drdhaval2785 <drdhaval2785@gmail.com>
2020-06-19 10:06:03 +0530

    regen 19062020

--- redo.sh
+++ redo.sh
@@ -3,11 +3,12 @@ echo "It presumes that the hwnorm1 repository is sibling of cologne-stardict rep
 cd ../hwnorm1
 git pull origin master
 cd ../cologne-stardict
-cp ../hwnorm1/ejf/hwnorm1c/hwnorm1c.txt input/hwnorm1c.txt
+cp ../hwnorm1/sanhw1/hwnorm1c.txt input/hwnorm1c.txt
 
 dictList=(acc ae ap ap90 ben bhs bop bor bur cae ccs gra gst ieg inm krm mci md mw mw72 mwe pd pe pgn pui pw pwg sch shs skd snp stc vcp vei wil yat)
 for Val in "${dictList[@]}"
 do
 	echo 'Started' $Val 'handling'.
-	python27 make_babylon.py input/extracted/pywork $Val $1
+	# python27 make_babylon.py input/extracted/pywork $Val $1
+	python2 make_babylon.py input/extracted/pywork $Val $1
 done
