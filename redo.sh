2274f3c95e28cd81306d00be09f9f27086390bf9
drdhaval2785 <drdhaval2785@gmail.com>
2021-08-16 06:32:22 +0530

    redo.sh

--- redo.sh
+++ redo.sh
@@ -4,7 +4,7 @@ git pull origin master
 cd ../cologne-stardict
 cp ../hwnorm1/sanhw1/hwnorm1c.txt input/hwnorm1c.txt
 
-dictList=(acc ae ap ap90 ben bhs bop bor bur cae ccs gra gst ieg inm krm mci md mw mw72 mwe pd pe pgn pui pw pwg sch shs skd snp stc vcp vei wil yat)
+dictList=(acc ae ap ap90 ben bhs bop bor bur cae ccs gra gst ieg inm krm mci md mw mw72 mwe pd pe pgn pui pw pwg sch shs skd snp stc vcp vei wil yat armh)
 for Val in "${dictList[@]}"
 do
 	echo 'Started' $Val 'handling'.
