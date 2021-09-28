a95efd5936aa43f6d839c1234c06f9c013a6c63b
drdhaval2785 <drdhaval2785@gmail.com>
2021-09-28 21:20:32 +0530

    BHS 1

--- utils.py
+++ utils.py
@@ -75,6 +75,6 @@ def devaconvert(line, dictId):
     line = line.replace('<div n="2"', '\n\t<div n="2"')
     line = line.replace('<div n="3"', '\n\t\t<div n="3"')
     line = line.replace('<div n="4"', '\n\t\t\t<div n="4"')
-    line = line.replace('--', '—')
+    # line = line.replace('--', '—')
     return line
     
